6a71c53432d019bcb7f1faa717633b146699fc42
[IPSA] Chris de Claverie <declaverie@gmail.com>
2017-10-29 17:08:21 +0100

    Sidebar border color now adaptative

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -1,26 +1,27 @@
 <template>
-    <section class="sidebar">
+    <section class="sidebar" v-bind:class="borderColor">
         <img src="../assets/images/logo.png" alt="IPSA ONE logo" class="logo" @click.prevent="redirectToHome">
 
         <h1 class="title">Administration Network for Nanosat Association</h1>
 
         <nav>
-            <router-link :to="{name: 'dashboard'}" class="grey main">Dashboard</router-link>
-
             <ul>
-                <li>
-                    <router-link :to="{name: 'blog'}" class="blue">Blog</router-link>
+                <li @click="updateBorderColor('grey')">
+                    <router-link :to="{name: 'dashboard'}" class="grey main">Dashboard</router-link>
                 </li>
-                <li>
+                <li @click="updateBorderColor('blue')">
+                    <router-link  :to="{name: 'blog'}" class="blue">Blog</router-link>
+                </li>
+                <li @click="updateBorderColor('green')">
                     <router-link :to="{name: 'storage'}" class="green">Drive</router-link>
                 </li>
-                <li>
+                <li @click="updateBorderColor('purple')" >
                     <router-link :to="{name: 'forum'}" class="purple">Forum</router-link>
                 </li>
-                <li>
+                <li @click="updateBorderColor('yellow')">
                     <router-link :to="{name: 'events'}" class="yellow">Events</router-link>
                 </li>
-                <li>
+                <li @click="updateBorderColor('red')">
                     <router-link :to="{name: 'gantt'}" class="red">Gantt</router-link>
                 </li>
             </ul>
@@ -28,7 +29,7 @@
 
         <div class="bottom">
             <a class="button badge" @click.prevent="showNotifications"
-               :data-badge="(notifications) ? notifications : false">
+               :data-badge="(notifications) ? notifications : ''">
                 <i class="fa fa-bell" aria-hidden="true"></i>
             </a>
 
@@ -49,8 +50,10 @@
     export default {
         data() {
             return {
-                notifications: 5
+                notifications: 5,
+                borderColor: ''
             };
+            
         },
         methods: {
             redirectToHome() {
@@ -64,6 +67,10 @@
             },
             logout() {
                 this.notifications += 1;
+            },
+            updateBorderColor(color) {
+                console.log('called with '+color);
+                this.borderColor = color;
             }
         }
     };
