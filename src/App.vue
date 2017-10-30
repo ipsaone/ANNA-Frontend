f4ef607ca6eeef399e8a537dcd354282550c36f1
Clement Chandon <clement.chandon@ipsa.fr>
2017-10-30 12:15:57 +0000

    Create login page

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="app">
-        <notifications />
+      <notifications />
         <div v-if="this.$route.path !== '/login'">
             <sidebar></sidebar>
         </div>
--- src/pages/LoginPage.vue
+++ src/pages/LoginPage.vue
@@ -1,5 +1,23 @@
 <template>
+  <div class="background">
+
     <div class="login">
-        <h1>Login</h1>
+        <img src="../assets/images/logo.png" alt="IPSA ONE logo" class="logo" @click.prevent="redirectToHome">
+
+        <form class="connexion">
+          <span class= "first-title">A.N.N.A.</span>
+          <span class= "second-title"> Admin Network for Nanosat Association </span>
+
+          <input type="text" id="user" class="inp" placeholder="Login">
+          <input type="password" id="password" class=  "inp" placeholder="Password">
+         <div class="button">
+           <input class="button1" type="button" value="Mot de passe oublié">
+           <input class="button1" type="submit" value="Connexion">
+         </div>
+      </form>
+
+      <div class="copyright"> Copyright ©IPSA ONE </div>
+
     </div>
+  </div>
 </template>
