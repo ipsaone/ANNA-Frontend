e1b4f9e770d31f117a6c7a0b7ac2382275ccb520
[IPSA] Chris de Claverie <declaverie@gmail.com>
2018-08-22 03:30:32 +0200

    Added events admin table

--- src/pages/Admin/AdminPage.vue
+++ src/pages/Admin/AdminPage.vue
@@ -1,148 +1,171 @@
 <template>
-    <div class='admin' v-bind:class="{'basic-layout': show_add_mission || show_add_user}">
-        <section class="admin">
-            <div class="content">
-                <h1 class="section-title">Administration</h1>
-                <tabs>
-                    <tab name="Missions">
-                        <table>
-                            <tr>
-                                <th>Name</th>
-                                <th>Leader</th>
-                                <th>Budget</th>
-                                <th>Members</th>
-                                <th>Actions</th>
-                            </tr>
-                            <tr v-for="mission in missions" :key="mission.id">   
-                                <td> {{ mission.name }} </td>
-                                <td> {{ mission.leader }} </td>
-                                <td> {{ mission }} </td>
-                                <td> {{ mission.members.length }} </td>
-                                <td> <a>Manage members</a>, <a>Manage budget</a> </td>
-                            </tr>
-                            <tr>
-                                <td></td>
-                                <td></td>
-                                <td></td>
-                                <td></td>
-                                <td><a @click.prevent="show_add_mission = !show_add_mission">Add mission</a></td>
-                            </tr>
-                        </table>
-                    </tab>
+    <div class='admin' v-bind:class="{'basic-layout': show_add_mission || show_add_user || show_add_event}">
+        <section class="content">
+            <h1 class="section-title">Administration</h1>
+            <tabs>
+                <tab name="Missions">
+                    <table>
+                        <tr>
+                            <th>Name</th>
+                            <th>Leader</th>
+                            <th>Budget</th>
+                            <th>Members</th>
+                            <th>Actions</th>
+                        </tr>
+                        <tr v-for="mission in missions" :key="mission.id">   
+                            <td> {{ mission.name }} </td>
+                            <td> {{ mission.leader }} </td>
+                            <td> {{ mission }} </td>
+                            <td> {{ mission.members.length }} </td>
+                            <td> <a>Manage members</a>, <a>Manage budget</a> </td>
+                        </tr>
+                        <tr>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                            <td><a @click.prevent="show_add_mission = !show_add_mission">Add mission</a></td>
+                        </tr>
+                    </table>
+                </tab>
 
-                    <tab name="Logs">
-                        <table>
-                            <tr>
-                                <th>Name</th>
-                                <th>Date</th>
-                                <th>Description</th>
-                                <th>Linked data</th>
-                                <th>Actions</th>
-                            </tr>
-                            <tr v-for="log in logs" :key="log.id">   
-                                <td> {{ log.name }} </td>
-                                <td> {{ mission.date }} </td>
-                                <td> {{ mission.description }} </td>
-                                <td> [Linked things] </td>
-                                <td> [Actions] </td>
-                            </tr>
-                            <tr>
-                                <td></td>
-                                <td></td>
-                                <td></td>
-                                <td></td>
-                                <td><a>Add log</a></td>
-                            </tr>
-                        </table>
-                    </tab>
+                <tab name="Logs">
+                    <table>
+                        <tr>
+                            <th>Name</th>
+                            <th>Date</th>
+                            <th>Description</th>
+                            <th>Linked data</th>
+                            <th>Actions</th>
+                        </tr>
+                        <tr v-for="log in logs" :key="log.id">   
+                            <td> {{ log.name }} </td>
+                            <td> {{ mission.date }} </td>
+                            <td> {{ mission.description }} </td>
+                            <td> [Linked things] </td>
+                            <td> [Actions] </td>
+                        </tr>
+                        <tr>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                            <td><a>Add log</a></td>
+                        </tr>
+                    </table>
+                </tab>
 
-                    <tab name="Blog">
-                        <table>
-                            <tr>
-                                <th>Id</th>
-                                <th>Title</th>
-                                <th>Date</th>
-                                <th>Author</th>
-                                <th>Text</th>
-                                <th>Actions</th>
-                            </tr>
-                            <tr v-for="post in posts" :key="post.id">   
-                                <td> {{ post.id }} </td>
-                                <td> {{ post.title }} </td>
-                                <td> {{ post.createdAt }} </td>
-                                <td> {{ post.author.username }} </td>
-                                <td> {{ post.content }} </td>
-                                <td> 
-                                    <router-link :to="{name: 'readPost', params: {id: post.id}}">Show</router-link>, 
-                                    <router-link :to="{name: 'editPost', params: {id: post.id}}">Edit</router-link>, 
-                                    <a @click.prevent="delPost(post.title, post.id)">Delete</a> 
-                                </td>
-                            </tr>
-                            <tr>
-                                <td></td>
-                                <td></td>
-                                <td></td>
-                                <td></td>
-                                <td></td>
-                                <td><router-link :to="{name: 'newPost'}">New</router-link></td>
-                            </tr>
-                        </table>
-                    </tab>
+                <tab name="Blog">
+                    <table>
+                        <tr>
+                            <th>Id</th>
+                            <th>Title</th>
+                            <th>Date</th>
+                            <th>Author</th>
+                            <th>Text</th>
+                            <th>Actions</th>
+                        </tr>
+                        <tr v-for="post in posts" :key="post.id">   
+                            <td> {{ post.id }} </td>
+                            <td> {{ post.title }} </td>
+                            <td> {{ post.createdAt }} </td>
+                            <td> {{ post.author.username }} </td>
+                            <td> {{ post.content }} </td>
+                            <td> 
+                                <router-link :to="{name: 'readPost', params: {id: post.id}}">Show</router-link>, 
+                                <router-link :to="{name: 'editPost', params: {id: post.id}}">Edit</router-link>, 
+                                <a @click.prevent="delPost(post.title, post.id)">Delete</a> 
+                            </td>
+                        </tr>
+                        <tr>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                            <td><router-link :to="{name: 'newPost'}">New</router-link></td>
+                        </tr>
+                    </table>
+                </tab>
 
-                    <tab name="Groups">
-                        <table>
-                            <tr>
-                                <th>Name</th>
-                                <th>Members</th>
-                                <th>Actions</th>
-                            </tr>
-                            <tr v-for="group in groups" :key="group.id">   
-                                <td> {{ group.name }} </td>
-                                <td> {{ group.users.length }} </td>
-                                <td> [Actions] </td>
-                            </tr>
-                            <tr>
-                                <td></td>
-                                <td></td>
-                                <td><a>Add group</a></td>
-                            </tr>
-                        </table>
-                    </tab>
+                <tab name="Groups">
+                    <table>
+                        <tr>
+                            <th>Name</th>
+                            <th>Members</th>
+                            <th>Actions</th>
+                        </tr>
+                        <tr v-for="group in groups" :key="group.id">   
+                            <td> {{ group.name }} </td>
+                            <td> {{ group.users.length }} </td>
+                            <td> <a>Manage users</a>, <a>Delete</a> </td>
+                        </tr>
+                        <tr>
+                            <td></td>
+                            <td></td>
+                            <td><a>Add group</a></td>
+                        </tr>
+                    </table>
+                </tab>
 
-                    <tab name="Users">
-                        <table>
-                            <tr>
-                                <th>ID</th>
-                                <th>Name</th>
-                                <th>Email</th>
-                                <th>Groups</th>
-                                <th>Actions</th>
-                            </tr>
-                            <tr v-for="user in users" :key="user.id">   
-                                 <td> {{ user.id }} </td>
-                                <td> {{ user.username }} </td>
-                                <td> {{ user.email }} </td>
-                                <td> {{ user.groups }} </td>
-                                <td> <a>Manage groups</a>, <a>Change password</a>, <a @click.prevent="delUser(user.username, user.id)">Delete</a> </td>
-                            </tr>
-                            <tr>
-                                <td></td>
-                                <td></td>
-                                <td></td>
-                                <td></td>
-                                <td><a @click.prevent="show_add_user = !show_add_user">Add user</a></td>
-                            </tr>
-                        </table>
-                        
-                    </tab>
+                <tab name="Users">
+                    <table>
+                        <tr>
+                            <th>ID</th>
+                            <th>Name</th>
+                            <th>Email</th>
+                            <th>Groups</th>
+                            <th>Actions</th>
+                        </tr>
+                        <tr v-for="user in users" :key="user.id">   
+                                <td> {{ user.id }} </td>
+                            <td> {{ user.username }} </td>
+                            <td> {{ user.email }} </td>
+                            <td> {{ user.groups }} </td>
+                            <td> <a>Manage groups</a>, <a>Change password</a>, <a @click.prevent="delUser(user.username, user.id)">Delete</a> </td>
+                        </tr>
+                        <tr>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                            <td><a @click.prevent="show_add_user = !show_add_user">Add user</a></td>
+                        </tr>
+                    </table>
+                </tab>
 
-                    <tab name="Events">
-                    </tab>
-                </tabs>
-            </div>
+                <tab name="Events">
+                    <table>
+                        <tr>
+                            <th>ID</th>
+                            <th>Name</th>
+                            <th>Description</th>
+                            <th>Registered</th>
+                            <th>Date</th>
+                            <th>Actions</th>
+                        </tr>
+                        <tr v-for="event in events" :key="event.id">   
+                            <td> {{ event.id }} </td>
+                            <td> {{ event.name }} </td>
+                            <td> {{ event.markdown }} </td>
+                            <td> ? / {{ event.maxRegistered }} </td>
+                            <td> {{ event.startDate }} - {{ event.endDate }} </td>
+                            <td> <a>Edit</a>, <a>Manage registered</a>, <a>Delete</a> </td>
+                        </tr>
+                        <tr>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                            <td><a @click.prevent="show_add_event = !show_add_event">Add event</a></td>
+                        </tr>
+                    </table>
+                </tab>
+            </tabs>
         </section>
 
-        <section v-if="show_add_user || show_add_mission" class="actions">
+        <section v-if="show_add_user || show_add_mission || show_add_event" class="actions">
             <h1 class="section-title">Actions</h1>
             
             <div v-if="show_add_user" class="add-user">
@@ -203,6 +226,42 @@
                     <button type="button" @click.prevent="newMission" class="submit">Submit</button>
                 </div>
             </div>
+
+            <div v-if="show_add_event" class="add-mission">
+                <div class="form-group">
+                    <label for="evt_name">Name :</label>
+                    <input type="text" name="evt_name" v-model="evt_name">
+                </div>
+
+                <div class="form-group">
+                    <label for="evt_desc">Description :</label>
+                    <input type="text" name="evt_desc" v-model="evt_desc">
+                </div>
+
+                <div class="form-group">
+                    <label for="evt_start">Start date :</label>
+                    <input type="date" name="evt_start" v-model="evt_start">
+                </div>
+
+                <div class="form-group">
+                    <label for="evt_end">End date :</label>
+                    <input type="date" name="evt_end" v-model="evt_end">
+                </div>
+
+                <div class="form-group">
+                    <label for="evt_max">Open slots :</label>
+                    <input type="number" name="evt_max" v-model="evt_max">
+                </div>
+
+                
+
+                <div class="buttons">
+                    <button type="button" @click.prevent="show_add_event = !show_add_event" class="cancel">Cancel</button>
+                    <button type="button" @click.prevent="newEvent" class="submit">Submit</button>
+                </div>
+            </div>
+
+
         </section>
     </div>
 </template>
@@ -231,8 +290,15 @@
                 miss_budget: 0,
                 miss_desc: '',
                 miss_group: 1,
+                evt_name: '',
+                evt_start: '',
+                evt_end: '',
+                evt_max: 0,
+                evt_name: '',
+                evt_desc: '',
                 show_add_mission: false,
                 show_add_user: false,
+                show_add_event: false,
                 panel_title: 'Actions'
             };
         },
@@ -251,6 +317,9 @@
             },
             missions() {
                 return store.getters.missions;
+            },
+            events() {
+                return store.getters.events;
             }
         },
         mounted() {
@@ -309,8 +378,51 @@
         },
         methods: {
             newGroup() {
-                if (group_name)
-                    GroupApi.create(group_name);
+                if (this.group_name) {
+                    this.loading = true;
+                    GroupApi.create(this.group_name)
+                        .then(() => {
+                            this.loading = false;
+                        })
+                        .then(() => this.$notify({
+                            type: 'success',
+                            title: 'Operation successful',
+                            text: 'Group was successfully added',
+                            duration: 5000
+                        }))
+                        .catch(err => this.$notify({
+                            type: 'error',
+                            title: 'Operation failed',
+                            text: err,
+                            duration: 5000
+                        }));
+                }
+            },
+
+            newEvent() {
+                this.loading = true;
+                store.dispatch('storeEvent', {
+                    name: this.evt_name,
+                    markdown: this.evt_desc,
+                    maxRegistered: this.evt_max,
+                    startDate: this.evt_start,
+                    endDate: this.evt_end
+                })
+                    .then(() => {
+                        this.loading = false;
+                    })
+                    .then(() => this.$notify({
+                        type: 'success',
+                        title: 'Operation successful',
+                        text: 'Mission was successfully added',
+                        duration: 5000
+                    }))
+                    .catch(err => this.$notify({
+                        type: 'error',
+                        title: 'Operation failed',
+                        text: err,
+                        duration: 5000
+                    }));
             },
 
             newMission() {
