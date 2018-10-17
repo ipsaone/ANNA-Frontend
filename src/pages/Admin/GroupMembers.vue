6f0d1748be01b6347414d3244986167d5b3f0363
Clement Chandon <clement.chandon@ipsa.fr>
2018-10-17 08:48:42 +0200

    grid for members management

--- src/pages/Admin/GroupMembers.vue
+++ src/pages/Admin/GroupMembers.vue
@@ -4,14 +4,25 @@
             <h1>Members Management</h1>
             <h2>Group: {{ group.name }}</h2>
 
+            <div class="lists-wrapper">
+                <h2>Users</h2>
+                <h2>Members</h2>
+                <form class="" action="" method="post">
+                    <ul class="users-list">
+                      <a v-for="user in users" v-on:click="addUser(user.id)">
+                          {{user.username}}
+                      </a>
+                    </ul>
+                </form>
+                <form class="" action="" method="post">
+                  <ul class="members-list">
+                      <a v-for="member in members" v-on:click="rmUser(user.id)">
+                          {{member.username}}
+                      </a>
+                  </ul>
+                </form>
 
-            <form class="" action="" method="post">
-                <ul>
-                  <li v-for="user in users">{{user.username}}<input type="checkbox" name=user.username></li>
-                </ul>
-
-            </form>
-
+            </div>
 
             <button type="submit" class="button success" @click.prevent="onSubmit">Submit</button>
         </div>
@@ -33,6 +44,9 @@
             },
             groups() {
                 return store.getters.groups;
+            },
+            members() {
+                [{username: 'moi'}, {username: 'toi'}];
             }
         },
         data() {
