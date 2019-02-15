faccecfef2e0d9fd3042b78544c53ddc8c26d819
Clement Chandon <clement.chandon@ipsa.fr>
2019-02-15 13:10:35 +0100

    WIP permissions

--- src/pages/Drive/ManagePermissions.vue
+++ src/pages/Drive/ManagePermissions.vue
@@ -1,21 +1,30 @@
 <template>
     <modal name="managePermissions" height="auto" :scrollable="true">
         <div class="content anna-modal">
-            <div class="tableau-permission">
-                <div class="case" id="owner">
-                    owner
-                </div>
-                <div class="case" id="group">
-                    group
-                </div>
-                <div class="case" id="all">
-                    all
-                </div>
-                <div class="case" id="read">
-                    read
-                </div>
-                <div class="case" id="write">
-                    write
+            <div class="big-wrapper">
+                <div class="tableau-permission">
+                    <div class="case" id="diagonale"></div>
+                    <div class="case" id="owner">
+                        owner
+                    </div>
+                    <div class="case" id="group">
+                        group
+                    </div>
+                    <div class="case" id="others">
+                        others
+                    </div>
+                    <div class="case" id="read">
+                        read
+                    </div>
+                    <div class="case" id="write">
+                        write
+                    </div>
+                    <div v-for="number in cases" class="case cochable">
+                        <div class="checkbox-container">
+                            <input type="checkbox" :name="number" :id="cases.indexOf(number)">
+                            <label class="checkbox" :for="cases.indexOf(number)"></label>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
@@ -23,9 +32,14 @@
 </template>
 
 <script>
-import store from '@/modules/store';
-import driveApi from '@/modules/drive/drive_api';
+    import store from '@/modules/store';
+    import driveApi from '@/modules/drive/drive_api';
 
-export default {
-};
+    export default {
+        computed : {
+            cases() {
+                return Array(1, 2, 3, 4, 5, 6);
+            }
+        }
+    };
 </script>
