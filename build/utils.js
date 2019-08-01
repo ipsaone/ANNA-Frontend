b5aa1bd44721bf20bd834f6323db4c0cef3b3d5f
kmgx <kmgx@declaverie.tech>
2019-08-01 12:21:14 +0200

    Update css-loader and url-loader

--- build/utils.js
+++ build/utils.js
@@ -15,7 +15,6 @@ exports.cssLoaders = function (options) {
   const cssLoader = {
     loader: 'css-loader',
     options: {
-      minimize: process.env.NODE_ENV === 'production',
       sourceMap: options.sourceMap
     }
   }
