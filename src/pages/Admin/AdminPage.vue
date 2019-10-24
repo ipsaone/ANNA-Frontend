3b92ed3c91ed8ec0246dd9de565240d50ed30e77
M1nlex <aleandrepe@gmail.com>
2019-10-24 16:27:41 +0200

    remove alert
    adaptative remove alert for admin page.

--- src/pages/Admin/AdminPage.vue
+++ src/pages/Admin/AdminPage.vue
@@ -234,6 +234,7 @@
 
     import * as moment from 'moment';
     import * as _ from 'lodash';
+    import swal from 'sweetalert2';
 
     export default {
         components: {
@@ -288,7 +289,7 @@
 
             },
             async delItem(type_name, action_name, item_name, item_id) {
-                if(confirm('Delete '+type_name+' "'+item_name+'" ?')) {
+                /*if(confirm('Delete '+type_name+' "'+item_name+'" ?')) {
                     this.loading = true;
                     await store.dispatch(action_name, item_id);
                     await this.refreshAll();
@@ -299,7 +300,25 @@
                         duration: 5000
                     });
 
-                }
+                }*/
+                swal({
+                    title: 'Remove '+type_name+'?',
+                    type: 'warning',
+                    showCancelButton: true,
+                    confirmButtonColor: '#E74D3C',
+                    cancelButtonColor: '#7A7A7A',
+                    confirmButtonText: 'Delete'
+                }).then(_ => {
+                    this.loading = true;
+                    store.dispatch(action_name, item_id);
+                    this.refreshAll();
+                    this.$notify({
+                        type: 'success',
+                        title: 'Operation successful',
+                        text: type_name+' was successfully deleted',
+                        duration: 5000
+                    });
+                });
             }
 
         }
