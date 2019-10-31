b9d321eaf5251a6db3fbc71d10793a4e6b8cc5b8
kmgx <kmgx@declaverie.tech>
2019-10-31 20:23:04 +0800

    Fix test branch permissions and code

--- scripts/test_branch.sh
+++ scripts/test_branch.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
-if [[ "$TRAVIS_BRANCH" != "master" || "$TRAVIS_BRANCH" != "staging" ]]; then
+if [[ "$TRAVIS_BRANCH" != "master" && "$TRAVIS_BRANCH" != "staging" ]]; then
   echo "We're not on the master or staging branch."
-  exit 0
+  exit -1
 fi
+exit 0
