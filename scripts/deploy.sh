8ad260e619ccef65ed08084bbf0287ab16edd7a8
kmgx <kmgx@declaverie.tech>
2019-11-24 02:12:38 +0800

    Second try

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,12 +1,4 @@
 #!/bin/bash
-openssl aes-256-cbc -K $encrypted_2861304911d5_key -iv $encrypted_2861304911d5_iv -in travis_deploy_key.enc -out ~/travis_deploy_key -d
-eval "$(ssh-agent -s)"
-chmod 600 ~/travis_deploy_key
-echo -e "Host ipsaone.space\n\tStrictHostKeyChecking no\n" >> ~/.ssh/config
-ssh-add ~/travis_deploy_key
-ssh -i ~/travis_deploy_key travis@ipsaone.space pwd
-ssh -i ~/travis_deploy_key travis@ipsaone.space echo $TRAVIS_BRANCH
-
 set -o verbose
 
 
@@ -14,6 +6,14 @@ if [[ "$TRAVIS_BRANCH" != "master" && "$TRAVIS_BRANCH" != "staging" ]]; then
   echo "We're not on the master or staging branch."
   exit -1
 fi
+
+openssl aes-256-cbc -K $encrypted_2861304911d5_key -iv $encrypted_2861304911d5_iv -in travis_deploy_key.enc -out ~/travis_deploy_key -d
+eval "$(ssh-agent -s)"
+chmod 600 ~/travis_deploy_key
+echo -e "Host ipsaone.space\n\tStrictHostKeyChecking no\n" >> ~/.ssh/config
+ssh-add ~/travis_deploy_key
+ssh -i ~/travis_deploy_key travis@ipsaone.space pwd
+ssh -i ~/travis_deploy_key travis@ipsaone.space echo $TRAVIS_BRANCH
 
 zip -r ./dist.zip ./dist
 scp -i ~/travis_deploy_key ./dist.zip travis@ipsaone.space:~/dist-$TRAVIS_BRANCH-deploying.zip
