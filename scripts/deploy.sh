03b2ed35fecad554304dc12c507f6e9a7770c726
M1nlex <47282781+M1nlex@users.noreply.github.com>
2020-04-19 14:33:46 +0200

    Fixing deploy.sh bug for second deployment.

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -22,7 +22,7 @@ ssh -i ~/travis_deploy_key travis@ipsaone.space echo $TRAVIS_BRANCH
 
 zip -r ./dist.zip ./dist
 scp -i ~/travis_deploy_key ./dist.zip travis@ipsaone.space:~/dist-$TRAVIS_BRANCH-deploying.zip
-ssh -i ~/travis_deploy_key travis@ipsaone.space -- unzip ~/dist-$TRAVIS_BRANCH-deploying.zip -d ~/ANNA-Frontend-$TRAVIS_BRANCH-new
+ssh -i ~/travis_deploy_key travis@ipsaone.space -- unzip -o ~/dist-$TRAVIS_BRANCH-deploying.zip -d ~/ANNA-Frontend-$TRAVIS_BRANCH-new
 ssh -i ~/travis_deploy_key travis@ipsaone.space -- mv ~/ANNA-Frontend-$TRAVIS_BRANCH ~/ANNA-Frontend-$TRAVIS_BRANCH-old
 ssh -i ~/travis_deploy_key travis@ipsaone.space -- mv ~/ANNA-Frontend-$TRAVIS_BRANCH-new/dist ~/ANNA-Frontend-$TRAVIS_BRANCH
 ssh -i ~/travis_deploy_key travis@ipsaone.space -- rm -rf ~/dist-$TRAVIS_BRANCH-deploying.zip ~/ANNA-Frontend-$TRAVIS_BRANCH-old ~/ANNA-Frontend-$TRAVIS_BRANCH-new
