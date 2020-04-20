af26fca78ca298162dee7c6a8b31b2fed8751566
[IPSA] Chris de Claverie <c.de-claverie@protonmail.com>
2020-04-20 16:17:35 -0400

    Selenium (#43)
    * Complete login test
    
    * Screenshot test
    
    * Write first test !
    
    * minor fixes
    
    Co-authored-by: kmgx <kmgx@declaverie.tech>
    Co-authored-by: clementchdn <35687688+clementchdn@users.noreply.github.com>
    Co-authored-by: MagixInTheAir <c.de-claverie@pm.me>

---
+++ src/pages/Login/test.js
@@ -0,0 +1,27 @@
+const webdriver = require('selenium-webdriver');
+const firefox = require('selenium-webdriver/firefox');
+const test = require('ava');
+
+let options = new firefox.Options();
+options.addArguments("-headless");
+
+const driver = new webdriver.Builder().forBrowser('firefox').setFirefoxOptions(options).build();
+
+
+test('root login works', async t => {
+    await driver.get('http://localhost:8080/login');
+
+    await driver.findElement(webdriver.By.id('username')).sendKeys('root');
+    await driver.sleep(100);
+    await driver.findElement(webdriver.By.id('password')).sendKeys('OneServ_2017');
+    await driver.sleep(100);
+
+    await driver.findElement(webdriver.By.id('login_btn')).click();
+    await driver.sleep(2000);
+    
+    let url = await driver.getCurrentUrl();
+    t.is(url, 'http://localhost:8080/dashboard');
+
+    await driver.quit();
+
+});
